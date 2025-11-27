ba4b3128054d0501387c8a358c188f77409cc64e
Rafael Mukhametov <rafaelwww07@mail.ru>
2025-11-27 20:14:44 +0100

    Initial Commit

---
+++ HabitTracker/HabitTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitTrackerApp.swift
+//  HabitTracker
+//
+//  Created by Rafael Mukhametov on 27.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct HabitTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
